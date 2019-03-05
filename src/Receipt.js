1369eb27eca75f8777c4c3a013628f447d246003
zhaohuanhuan <1297972106@qq.com>
2019-03-05 11:08:44 +0800

    [refactor]zhh

--- src/Receipt.js
+++ src/Receipt.js
@@ -1,11 +1,11 @@
 const FIXED_COST = 50;
 const PEAK_TIME_MULTIPLIER = 1.2;
 const OFF_PEAK_MULTIPLIER = 1.0;
-const RATE_CHANGE_DISTANCE = 10;
-const PRE_RATE_CHANGE_NON_AC_RATE = 15;
-const POST_RATE_CHANGE_NON_AC_RATE = 12;
-const PRE_RATE_CHANGE_AC_RATE = 20;
-const POST_RATE_CHANGE_AC_RATE = 17;
+const RATE_CHARGE_DISTANCE = 10;
+const PRE_RATE_CHARGE_NON_AC_RATE = 15;
+const POST_RATE_CHARGE_NON_AC_RATE = 12;
+const PRE_RATE_CHARGE_AC_RATE = 20;
+const POST_RATE_CHARGE_AC_RATE = 17;
 const SALES_TAX_RATE = 0.1;
 
 class Receipt {
@@ -27,17 +27,23 @@ class Receipt {
   calculateTaxiCost() {
     let taxiCost = 0;
     let totalKms = this.taxi.getTotalKms();
-    let peakTimeMultiple = this.taxi.isPeakTime() ? PEAK_TIME_MULTIPLIER : OFF_PEAK_MULTIPLIER;
-    let PreChargingInterval = Math.min(RATE_CHANGE_DISTANCE, totalKms);
-    let PostChargingInterval = Math.max(0, totalKms - RATE_CHANGE_DISTANCE);
-    let PreTaxiCost = 0;
-    let PostTaxiCost = 0;
+    let peakTimeMultiple = this.getPeakTimeMultiple();
+    let preChargingInterval = Math.min(RATE_CHARGE_DISTANCE, totalKms);
+    let postChargingInterval = Math.max(0, totalKms - RATE_CHARGE_DISTANCE);
 
-    PreTaxiCost = PreChargingInterval * (this.taxi.isAirConditioned() ? PRE_RATE_CHANGE_AC_RATE : PRE_RATE_CHANGE_NON_AC_RATE);
-    PostTaxiCost = PostChargingInterval * (this.taxi.isAirConditioned() ? POST_RATE_CHANGE_AC_RATE : POST_RATE_CHANGE_NON_AC_RATE);
-    taxiCost = (PreTaxiCost + PostTaxiCost) * peakTimeMultiple;
+    taxiCost = preChargingInterval * this.getPreACRate() + postChargingInterval * this.getPostACRate()
 
-    return taxiCost;
+    return taxiCost * peakTimeMultiple;
+  }
+
+  getPeakTimeMultiple() {
+    return this.taxi.isPeakTime() ? PEAK_TIME_MULTIPLIER : OFF_PEAK_MULTIPLIER;
+  }
+  getPreACRate() {
+    return this.taxi.isAirConditioned() ? PRE_RATE_CHARGE_AC_RATE : PRE_RATE_CHARGE_NON_AC_RATE;
+  }
+  getPostACRate() {
+    return this.taxi.isAirConditioned() ? POST_RATE_CHARGE_AC_RATE : POST_RATE_CHARGE_NON_AC_RATE;
   }
 }
 
