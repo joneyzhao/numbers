1de77d0f41ee171d1eed31c5c34cf9668df9a947
zhaohuanhuan <1297972106@qq.com>
2019-03-05 14:54:24 +0800

    transfer fn 2

--- src/Receipt.js
+++ src/Receipt.js
@@ -1,5 +1,4 @@
 const FIXED_COST = 50;
-const RATE_CHARGE_DISTANCE = 10;
 const SALES_TAX_RATE = 0.1;
 
 class Receipt {
@@ -8,34 +7,11 @@ class Receipt {
   }
 
   getTotalCost() {
-    let totalCost = 0;
-    // fixed cost
-    totalCost += FIXED_COST;
+    let totalCost = FIXED_COST;
 
-    // add taxi cost
-    totalCost += this.calculateTaxiCost();
+    totalCost += this.taxi.calculateTaxiCost();
 
     return totalCost * (1 + SALES_TAX_RATE);
-  }
-
-  calculateTaxiCost() {
-    let peakTimeMultiple = this.taxi.getPeakTimeMultiple();
-    let preTaxiCost = this.getPreTaxiCost();
-    let postTaxiCost = this.getPostTaxiCost();
-    
-    return (preTaxiCost + postTaxiCost) * peakTimeMultiple;
-  }
-
-  getPreTaxiCost() {
-    let totalKms = this.taxi.getTotalKms();
-    let preChargingInterval = Math.min(RATE_CHARGE_DISTANCE, totalKms);
-    return preChargingInterval * this.taxi.getPreACRate();
-  }
-
-  getPostTaxiCost() {
-    let totalKms = this.taxi.getTotalKms();
-    let postChargingInterval = Math.max(0, totalKms - RATE_CHARGE_DISTANCE);
-    return postChargingInterval * this.taxi.getPostACRate();
   }
 
 }
