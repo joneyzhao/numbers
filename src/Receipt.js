96a2e21716e6efa0ff2997e3efc9d33d7399a7e9
zhaohuanhuan <1297972106@qq.com>
2019-03-05 10:32:01 +0800

    [refactor] rename3

--- src/Receipt.js
+++ src/Receipt.js
@@ -1,11 +1,11 @@
 const FIXED_COST = 50;
 const PEAK_TIME_MULTIPLIER = 1.2;
 const OFF_PEAK_MULTIPLIER = 1.0;
-const RATE_COST_DISTANCE = 10;
-const PRE_RATE_COST_NON_AC_RATE = 15;
-const POST_RATE_COST_NON_AC_RATE = 12;
-const PRE_RATE_COST_AC_RATE = 20;
-const POST_RATE_COST_AC_RATE = 17;
+const RATE_CHANGE_DISTANCE = 10;
+const PRE_RATE_CHANGE_NON_AC_RATE = 15;
+const POST_RATE_CHANGE_NON_AC_RATE = 12;
+const PRE_RATE_CHANGE_AC_RATE = 20;
+const POST_RATE_CHANGE_AC_RATE = 17;
 const SALES_TAX_RATE = 0.1;
 
 class Receipt {
@@ -28,14 +28,13 @@ class Receipt {
     let taxiCost = 0;
     let totalKms = this.taxi.getTotalKms();
     let peakTimeMultiple = this.taxi.isPeakTime() ? PEAK_TIME_MULTIPLIER : OFF_PEAK_MULTIPLIER;
-    
     if (this.taxi.isAirConditioned()) {
-      taxiCost = Math.min(RATE_COST_DISTANCE, totalKms) * PRE_RATE_COST_AC_RATE;
-      taxiCost += Math.max(0, totalKms - RATE_COST_DISTANCE) * POST_RATE_COST_AC_RATE;
+      taxiCost = Math.min(RATE_CHANGE_DISTANCE, totalKms) * PRE_RATE_CHANGE_AC_RATE;
+      taxiCost += Math.max(0, totalKms - RATE_CHANGE_DISTANCE) * POST_RATE_CHANGE_AC_RATE;
     }
     else {
-      taxiCost = Math.min(RATE_COST_DISTANCE, totalKms) * PRE_RATE_COST_NON_AC_RATE;
-      taxiCost += Math.max(0, totalKms - RATE_COST_DISTANCE) * POST_RATE_COST_NON_AC_RATE;
+      taxiCost = Math.min(RATE_CHANGE_DISTANCE, totalKms) * PRE_RATE_CHANGE_NON_AC_RATE;
+      taxiCost += Math.max(0, totalKms - RATE_CHANGE_DISTANCE) * POST_RATE_CHANGE_NON_AC_RATE;
     }
     return taxiCost * peakTimeMultiple;
   }
