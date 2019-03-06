e03c9867712d56fb274e433752b7163187100040
zhaohuanhuan <1297972106@qq.com>
2019-03-06 13:19:32 +0800

    function succ

--- src/Mommfier.js
+++ src/Mommfier.js
@@ -1,32 +1,47 @@
 const Mommfier = {
   mommify: (str) => {
+    console.log('===str==' + str)
     let vowelsCount = 0;
     let vowelRate = 0;
     vowelsCount = (str.match(/[aeiou]/ig) || []).length;
-    vowelRate = vowelRate/str.length;
-
+    vowelRate = vowelsCount/str.length;
+    console.log('===vowelRate==' + vowelRate)
+    console.log('===vowelsCount==' + vowelsCount)
     if (vowelsCount < 1 || vowelRate < 0.3) {
       return str;
     } else {
-      if (list.length == 1) {
+      console.log('===str.length==' + str.length)
+      if (str.length == 1) {
+        console.log('===vowelsCount=22=')
         return 'mommify';
       } else {
         let list = str.split('');
-        for (let i = 1; i < list.length; i++) {
-          if (isVowel(list[i])) {
-            if (!isVowel(list[i-1])) {
-              list[i] = 'mommify';
+        console.log('===listLength==' + list.length)
+        for (let i = list.length-1; i >= 0; i--) {
+          console.log('==isVowel(list[i])==' + Mommfier.isVowel(list[i]))
+          if (Mommfier.isVowel(list[i])) {
+            if (i > 0) {
+              if (!Mommfier.isVowel(list[i-1])) {
+                list[i] = 'mommify';
+              } else {
+                list.splice(i,1);
+              }
             } else {
-              list.splice(i,1);
+              list[i] = 'mommify';
             }
           }
         }
+        console.log('===list.join===' + list.join(''))
         return list.join('');
       }
     }
   },
   isVowel: (s) => {
-    return (s.match(/[aeiou]/ig) == true);
+    if (s.match(/[aeiou]/ig) && '' !== s.match(/[aeiou]/ig)) {
+      return true
+    } else {
+      return false
+    }
   }
 }
 
