fdc16e44af32cff5a470ac3d6242ac84ff592dde
zhaohuanhuan <1297972106@qq.com>
2019-03-07 11:59:25 +0800

    [refactor] fn modifiedStr

--- src/Mommfier.js
+++ src/Mommfier.js
@@ -11,50 +11,21 @@ const Mommfier = {
   modifiedStr: (str) => {
     let list = str.split('');
     let expectStr = [];
-    
+    let isContinuousVowel = false;
+
     for (let i = 0; i < list.length; i++) {
       if (Mommfier.isVowel(list[i])) {
-        expectStr[expectStr.length] = 'mommify';
-        console.log('==expectStr==1===' + expectStr);
-        // Mommfier.stopUntilNotIsVowel(i);
-        for (let j = i+1; j < list.length; j++) {
-          if (Mommfier.isVowel(list[j])) {
-            console.log('==expectStr==2===' + expectStr);
-            continue;
-          } 
-          else {
-            i = j;
-            expectStr[expectStr.length] = list[i];
-            console.log('==expectStr==3===' + expectStr);
-            continue;
-          }
-        } 
-        break;
+        if (!isContinuousVowel) {
+          expectStr[expectStr.length] = 'mommify';
+          isContinuousVowel = true;
+        }
       } else {
+        isContinuousVowel = false;
         expectStr[expectStr.length] = list[i];
-        console.log('==expectStr==4===' + expectStr);
       }
     }
     
     return expectStr.join('');
-
-    // for (let i = list.length - 1; i >= 0; i--) {
-    //   if (Mommfier.isVowel(list[i])) {
-    //     if (i > 0) {
-    //       if (!Mommfier.isVowel(list[i - 1])) {
-    //         list[i] = 'mommify';
-    //       } else {
-    //         list.splice(i, 1);
-    //       }
-    //     } else {
-    //       list[i] = 'mommify';
-    //     }
-    //   }
-    // }
-    // return list.join('');
-  },
-  stopUntilNotIsVowel: (i) => {
-
   },
   isVowel: (s) => {
     return s.match(/[aeiou]/ig) ? true : false;
