34097579c697752f36d51e1d8b1af1fe25be92b6
zhaohuanhuan <1297972106@qq.com>
2019-03-07 09:46:45 +0800

    [refactor] fn split

--- src/Mommfier.js
+++ src/Mommfier.js
@@ -1,47 +1,34 @@
 const Mommfier = {
   mommify: (str) => {
-    console.log('===str==' + str)
     let vowelsCount = 0;
-    let vowelRate = 0;
+    let vowelsRate = 0;
     vowelsCount = (str.match(/[aeiou]/ig) || []).length;
-    vowelRate = vowelsCount/str.length;
-    console.log('===vowelRate==' + vowelRate)
-    console.log('===vowelsCount==' + vowelsCount)
-    if (vowelsCount < 1 || vowelRate < 0.3) {
+    vowelsRate = vowelsCount/str.length;
+    if (vowelsCount < 1 || vowelsRate < 0.3) {
       return str;
     } else {
-      console.log('===str.length==' + str.length)
-      if (str.length == 1) {
-        console.log('===vowelsCount=22=')
-        return 'mommify';
-      } else {
-        let list = str.split('');
-        console.log('===listLength==' + list.length)
-        for (let i = list.length-1; i >= 0; i--) {
-          console.log('==isVowel(list[i])==' + Mommfier.isVowel(list[i]))
-          if (Mommfier.isVowel(list[i])) {
-            if (i > 0) {
-              if (!Mommfier.isVowel(list[i-1])) {
-                list[i] = 'mommify';
-              } else {
-                list.splice(i,1);
-              }
-            } else {
-              list[i] = 'mommify';
-            }
-          }
-        }
-        console.log('===list.join===' + list.join(''))
-        return list.join('');
-      }
+      return Mommfier.modifiedStr(str);
     }
   },
-  isVowel: (s) => {
-    if (s.match(/[aeiou]/ig) && '' !== s.match(/[aeiou]/ig)) {
-      return true
-    } else {
-      return false
+  modifiedStr: (str) => {
+    let list = str.split('');
+    for (let i = list.length - 1; i >= 0; i--) {
+      if (Mommfier.isVowel(list[i])) {
+        if (i > 0) {
+          if (!Mommfier.isVowel(list[i - 1])) {
+            list[i] = 'mommify';
+          } else {
+            list.splice(i, 1);
+          }
+        } else {
+          list[i] = 'mommify';
+        }
+      }
     }
+    return list.join('');
+  },
+  isVowel: (s) => {
+    return s.match(/[aeiou]/ig) ? true : false;
   }
 }
 
