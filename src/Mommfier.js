38a16f1002dec9fa72fa8a3b64b7060ef715741c
zhaohuanhuan <1297972106@qq.com>
2019-03-07 17:13:04 +0800

    [refactor] refactor modifiedStr, add fn isLastContinuousVowel

--- src/Mommfier.js
+++ src/Mommfier.js
@@ -1,5 +1,6 @@
 const Mommfier = {
   mommify: (str) => {
+    console.log('str====' + str);
     let vowelsCount = (str.match(/[aeiou]/ig) || []).length;
     let vowelsRate = vowelsCount / str.length; 
     if (vowelsCount < 1 || vowelsRate < 0.3) {
@@ -10,22 +11,36 @@ const Mommfier = {
   },
   modifiedStr: (str) => {
     let list = str.split('');
-    let expectStr = [];
-    let isContinuousVowel = false;
+    let expectStr = '';
 
     for (let i = 0; i < list.length; i++) {
-      if (Mommfier.isVowel(list[i])) {
-        if (!isContinuousVowel) {
-          expectStr[expectStr.length] = 'mommify';
-          isContinuousVowel = true;
-        }
-      } else {
-        isContinuousVowel = false;
-        expectStr[expectStr.length] = list[i];
+      let currentChar = list[i];
+
+      console.log('i====' + i);
+      console.log('list====' + list);
+      if (!Mommfier.isVowel(currentChar)) {
+        expectStr += currentChar;
+      } else if (Mommfier.isLastContinuousVowel(i, list)) {
+        expectStr += 'mommify'
       }
     }
-    
-    return expectStr.join('');
+    console.log('expectStr====' + expectStr);
+
+    return expectStr;
+  },
+  isLastContinuousVowel: (index, list) => {
+    console.log('===index===' + index);
+    console.log('===list===' + list);
+    console.log('===list.length===' + list.length);
+    if (index >= list.length-1) {
+      return true;
+    } else {
+      if (Mommfier.isVowel(list[index + 1])) {
+          return false;
+      } else {
+          return true;
+      }
+    }
   },
   isVowel: (s) => {
     return s.match(/[aeiou]/ig) ? true : false;
