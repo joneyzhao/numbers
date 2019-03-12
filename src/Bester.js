96e6fa9e2a98d6b45bce45e537e834bb92e50003
zhaohuanhuan <1297972106@qq.com>
2019-03-12 11:22:59 +0800

    [rafactor] 多态实现

--- src/Bester.js
+++ src/Bester.js
@@ -1,6 +1,10 @@
 class Oreo {
   constructor(chocolateNumber) {
-    this.chocolateNumber = chocolateNumber
+    this.chocolateNumber = chocolateNumber;
+  }
+
+  isBetterThan(oreo) {
+    return this.chocolateNumber > oreo.chocolateNumber;
   }
 }
 
@@ -9,11 +13,19 @@ class TV {
     this.width = width;
     this.height = height;
   }
+
+  isBetterThan(tv) {
+    return this.width * this.height > tv.width * tv.height;
+  }
 }
 
 class Hall {
   constructor(radius) {
     this.radius = radius;
+  }
+
+  isBetterThan(hall) {
+    return this.radius > hall.radius;
   }
 }
 
@@ -21,26 +33,30 @@ const Bester = {
   chooseTheBest: (items) => { 
     var besterOne = items[0];
 
-    for (let i = 0; i < items.length - 1; i++) {
-      if (Bester.calculate(items[i]) < Bester.calculate(items[i+1])) {
-        besterOne = items[i+1];
+    for (let i = 1; i < items.length; i++) {
+      // if (Bester.calculate(items[i]) < Bester.calculate(items[i+1])) {
+      //   besterOne = items[i+1];
+      // }
+      console.log('besterOne.isBetterThan(items[i])===' + besterOne.isBetterThan(items[i]))
+      if (items[i].isBetterThan(besterOne)) {
+        besterOne = items[i];
       }
     }
     
     return besterOne;
   },
-  calculate: (item) => {
-    let result = 0;
+  // calculate: (item) => {
+  //   let result = 0;
 
-    if (item instanceof Hall) {
-      result = item.radius;
-    } else if (item instanceof TV) {
-      result = item.width * item.height;
-    } else {
-      result = item.chocolateNumber;
-    }
-    return result;
-  }
+  //   if (item instanceof Hall) {
+  //     result = item.radius;
+  //   } else if (item instanceof TV) {
+  //     result = item.width * item.height;
+  //   } else {
+  //     result = item.chocolateNumber;
+  //   }
+  //   return result;
+  // }
 }
 
 
