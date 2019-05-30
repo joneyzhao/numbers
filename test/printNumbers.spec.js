3a7751e6d94900b80e177b81e54b8b05970dd220
zhaohuanhuan <1297972106@qq.com>
2019-05-30 20:00:18 +0800

    pass 1,2...100

--- test/printNumbers.spec.js
+++ test/printNumbers.spec.js
@@ -11,25 +11,25 @@ describe('Should Style: Answer', () => {
     expect('1,2').to.equal(printNumbers.printsContents(2));
   });
 
-  // it('expect 3 is 1 2 Fizz', () => {
-  //   expect('1,2,Fizz').to.equal(printNumbers.printsContents(3));
-  // });
-  // it('expect 4 is 1 2 Fizz 4', () => {
-  //   expect('1,2,Fizz,4').to.equal(printNumbers.printsContents(4));
-  // });
+  it('expect 3 is 1 2 Fizz', () => {
+    expect('1,2,Fizz').to.equal(printNumbers.printsContents(3));
+  });
+  it('expect 4 is 1 2 Fizz 4', () => {
+    expect('1,2,Fizz,4').to.equal(printNumbers.printsContents(4));
+  });
 
-  // it('expect 5 is 1 2 Fizz 4 Buzz', () => {
-  //   expect('1,2,Fizz,4,Buzz').to.equal(printNumbers.printsContents(5));
-  // });
-  // it('expect 13 is **', () => {
-  //   expect('1,2,Fizz,4,Buzz,Fizz,7,8,Fizz,Buzz,11,Fizz,Fizz').to.equal(printNumbers.printsContents(13));
-  // });
-  // it('expect 15 is **', () => {
-  //   expect('1,2,Fizz,4,Buzz,Fizz,7,8,Fizz,Buzz,11,Fizz,Fizz,14,Fizz').to.equal(printNumbers.printsContents(15));
-  // });
+  it('expect 5 is 1 2 Fizz 4 Buzz', () => {
+    expect('1,2,Fizz,4,Buzz').to.equal(printNumbers.printsContents(5));
+  });
+  it('expect 13 is **', () => {
+    expect('1,2,Fizz,4,Buzz,Fizz,7,8,Fizz,Buzz,11,Fizz,Fizz').to.equal(printNumbers.printsContents(13));
+  });
+  it('expect 15 is **', () => {
+    expect('1,2,Fizz,4,Buzz,Fizz,7,8,Fizz,Buzz,11,Fizz,Fizz,14,Fizz').to.equal(printNumbers.printsContents(15));
+  });
 
-  // it('expect 100 length is 100', () => {
-  //   expect(100).to.equal(printNumbers.printsContents(100));
-  // });
+  it('expect 100 length is 100', () => {
+    expect(100).to.equal(printNumbers.printsContents(100));
+  });
 });
 
